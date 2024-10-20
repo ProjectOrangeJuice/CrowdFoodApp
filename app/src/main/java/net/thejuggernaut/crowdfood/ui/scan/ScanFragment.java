56468d941657d9d5f5882a288ce24d547751180a
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:18 +0100

    Opens camera

--- app/src/main/java/net/thejuggernaut/crowdfood/ui/scan/ScanFragment.java
+++ app/src/main/java/net/thejuggernaut/crowdfood/ui/scan/ScanFragment.java
@@ -1,12 +1,18 @@
 package net.thejuggernaut.crowdfood.ui.scan;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,11 +21,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import net.thejuggernaut.crowdfood.MainActivity;
 import net.thejuggernaut.crowdfood.R;
 import net.thejuggernaut.crowdfood.api.FoodieAPI;
 import net.thejuggernaut.crowdfood.api.Product;
 import net.thejuggernaut.crowdfood.api.SetupRetro;
+import net.thejuggernaut.crowdfood.ui.CameraPreview;
 import net.thejuggernaut.crowdfood.ui.Scanned;
+
+import java.io.IOException;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,15 +40,37 @@ public class ScanFragment extends Fragment {
 
     private ScanViewModel scanViewModel;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         scanViewModel =
                 ViewModelProviders.of(this).get(ScanViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scan, container, false);
         buttonActions(root);
+        //should be done on another thread
+        mCamera =  Camera.open();
+        mCamera.setDisplayOrientation(90);
+        cameraPreview = (LinearLayout) root.findViewById(R.id.la);
+        mPreview = new CameraPreview(getContext(), mCamera);
+        cameraPreview.addView(mPreview);
+        mCamera.startPreview();
+        mCamera = Camera.open(0);
+        mCamera.setDisplayOrientation(90);
+        mPicture = getPictureCallback();
+        mPreview.refreshCamera(mCamera);
         return root;
     }
-
+    private Camera.PictureCallback getPictureCallback() {
+        Camera.PictureCallback picture = new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                //Intent intent = new Intent(this,Cam.class);
+                //startActivity(intent);
+            }
+        };
+        return picture;
+    }
     public void buttonActions(View view) {
         Button scanButton = (Button) view.findViewById(R.id.scanButton);
         scanButton.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +80,22 @@ public class ScanFragment extends Fragment {
         });
 
     }
+
+
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Camera.PictureCallback mPicture;
+    private Button capture, switchCamera;
+    private Context myContext;
+    private LinearLayout cameraPreview;
+    private boolean cameraFront = false;
+    public static Bitmap bitmap;
+
+
+
+
+
 
     public void scanItem(final View view){
         String barcode = "000343";
