49cdb749a62784504402d4ba5607ce50e0b4beb1
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:18 +0100

    Corrected empty user profile

--- app/src/main/java/net/thejuggernaut/crowdfood/MainActivity.java
+++ app/src/main/java/net/thejuggernaut/crowdfood/MainActivity.java
@@ -1,15 +1,26 @@
 package net.thejuggernaut.crowdfood;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Window;
+import android.widget.Toast;
 
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationAPIClient;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.authentication.storage.SecureCredentialsManager;
+import com.auth0.android.authentication.storage.SharedPreferencesStorage;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.gson.Gson;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,17 +30,14 @@ import androidx.navigation.ui.NavigationUI;
 import net.thejuggernaut.crowdfood.accountApi.AccountApi;
 import net.thejuggernaut.crowdfood.accountApi.Info;
 import net.thejuggernaut.crowdfood.accountApi.SetupRetro;
-import net.thejuggernaut.crowdfood.ui.scan.DisplayProduct;
 
-import org.json.JSONObject;
-
-import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private Auth0 auth0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +56,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+
         profileRefresh();
     }
 
 
-
     private void profileRefresh(){
-        AccountApi accountAPI = SetupRetro.getRetro();
+        AccountApi accountAPI = SetupRetro.getRetro(this);
         Call<Info> call = accountAPI.loadAccount();
         call.enqueue(new Callback<Info>() { @Override
         public void onResponse(Call<Info> call, Response<Info> response) {
@@ -62,7 +72,10 @@ public class MainActivity extends AppCompatActivity {
 
                 //Store allergies
                 SharedPreferences pref = getSharedPreferences("AccountInfo",MODE_PRIVATE);
-                String allergies = TextUtils.join(",",response.body().getAllergies());
+                String allergies = "";
+                if(response.body().getAllergies() != null){
+                    allergies = TextUtils.join(",", response.body().getAllergies());
+                }
                 pref.edit().putString("Allergies",allergies).apply();
 
                 //Convert the recommended back to json!
